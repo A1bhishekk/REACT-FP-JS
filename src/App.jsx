b2e323f7882e0e4ae0d7ad62940c826b2faf9fc2
Abhishek Kumar <sahniak56@gmail.com>
2022-12-13 13:02:17 +0530

    added cs

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [screenh, setscreenh] = useState(null);
   const [screenw, setscreenw] = useState(null);
   const [os, setos] = useState(null);
-  const [cpu, setcpu] = useState(null);
+  const [confidence, setconfidence] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -23,31 +23,29 @@ function App() {
       setscreenh(fingerprint.components.screenResolution.value[0]);
       setscreenw(fingerprint.components.screenResolution.value[1]);
       setos(fingerprint.components.platform.value);
-      setcpu(fingerprint.components);
+      // setcpu(fingerprint.components);
+      setconfidence(fingerprint.confidence.score);
       console.log(fingerprint);
+      console.log(`confidence score: ${fingerprint.confidence.score}`)
     })();
   }, []);
 
- 
+
 
   return (
     <div className="App">
       <div className='container'>
-      <pre>
-      {
-        JSON.stringify(cpu,[])
-      }
-      </pre>
-        <h2>💕 Technical Abhi 💕</h2>
+        <h2> Technical Abhi </h2>
         {visitorId ? (
           <div>
-            <p>Your device 💻:{os}</p>
             <p>Visitor ID 📇:{visitorId}</p>
+            <p>Your device 💻:{os}</p>
             <p>Browser 🐇:{browser}</p>
             <p>Timezone ⏲️:{timezone}</p>
             <p>Screen Width:{screenw}</p>
             <p>Screen Height:{screenh}</p>
             <p>RAM: {ram}GB</p>
+            <p>Confidence Score:{confidence}</p>
           </div>
         ) : (
           <p>Technical Abhi detecting your browser's fingerprint.</p>
